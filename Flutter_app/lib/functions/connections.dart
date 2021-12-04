454e656abfd9215daf677b01c5b2664455625d28
EAgulto <edzel.agulto@gmail.com>
2021-12-04 20:53:14 +0800

    added transfer of trackmovement from phone to screen

---
+++ Flutter_app/lib/functions/connections.dart
@@ -0,0 +1,30 @@
+import 'dart:convert';
+
+import 'package:flutter/gestures.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+io.Socket socket = io.io('http://192.168.1.13:3000', <String, dynamic>{
+  'transports': ['websocket'],
+  'autoConnect': false,
+});
+
+class CommandTransfer {
+  void connect() {
+    print('hello');
+    socket.connect();
+    socket.emit('msg', 'hi');
+  }
+
+  void sendTrackMovement(Offset movement) {
+    var initialPosition = json.encode({
+      "x": movement.dx,
+      "y": movement.dy,
+    });
+    print(initialPosition);
+    socket.emit('onPanDown', initialPosition);
+  }
+
+  void sendPanEnd() {
+    socket.emit('onPanEnd', 'hands up');
+  }
+}
